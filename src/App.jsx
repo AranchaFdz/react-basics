aef93eed08ed306406338ab608bf5b35496582f9
Arancha Fernandez <aferfer1978@gmail.com>
2024-12-29 18:10:33 +0100

    Show how stles can be used external, modules and inline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+// How to style react components with CSS
+// ------------------------------------
+// ( not including external frameworks or preprocessors)
 
+// 1. External
+// 2. Modules
+// 3. Inline
+
+
+import Button from './Button/Button.jsx'
+import ButtonInline from './ButtonInline.jsx'
 
 function App() {
 
   return(
     <>
-    
+      <Button />
+      <hr />
+      <ButtonInline />
     </>
   )
 }
