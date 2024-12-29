ae816af18db6ba55903493532efa4d0a558a7969
Arancha Fernandez <aferfer1978@gmail.com>
2024-12-29 17:51:39 +0100

    eliminated bug in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import Food from "./Food"
+
 
 function App() {
 
   return(
     <>
-    <Header />
-    <Food />
-    <Footer />
+    
     </>
   )
 }
