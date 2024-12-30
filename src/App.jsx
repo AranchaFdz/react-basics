fc156ea243c817372bb797112108c20f540283c2
Arancha Fernandez <aferfer1978@gmail.com>
2024-12-30 18:02:02 +0100

    Conditional rendering done showing all three conditiones show, hide or change components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+// Conditional Rendering = allows you to control what gets rendered
+//                         in your application based on certain conditions
+//                         ( show, hide, or change components )
 
+import UserGreeting from "./UserGreeting"
 
 function App() {
 
   return(
     <>
-    
+    <UserGreeting isLoggedIn={true} username="Anaïs"/>
     </>
   )
 }
