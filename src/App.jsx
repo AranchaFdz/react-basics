c2f071fceac4ded326920cb0bcd7e8ab80e018c1
Arancha Fernandez <aferfer1978@gmail.com>
2025-01-04 00:52:51 +0100

    OnChange event handlers. Shown input, textarea, select and radio with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+// onchange = event handler used primarily with form elements
+//            ex. <input>, <select>, <textarea>, <radio>
+//            Triggers a function every time the value of the input changes
 
-
+import MyComponent from './MyComponent.jsx'
 function App() {
 
-  return(
-    <>
-    
-    </>
+  return (
+    <div>
+      <MyComponent />
+    </div>
   )
 }
 
