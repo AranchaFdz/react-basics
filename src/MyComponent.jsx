378bd12be8bb005d0c5bc33bf8445a2441f27522
Arancha Fernandez <aferfer1978@gmail.com>
2025-01-06 11:19:31 +0100

    UseRef completed

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -22,16 +22,17 @@ function MyComponent() {
         console.log("COMPONENT RENDERED")
     }) */
 
-    const inputRef = useRef(null)
+    const inputRef1 = useRef(null)
+    const inputRef2 = useRef(null)
+    const inputRef3 = useRef(null)
 
     //console.log(ref)
 
     useEffect(() => {
         console.log("COMPONENT RENDERED")
-        //console.log(inputRef)
     }) 
 
-    function handleClick() {
+    function handleClick1() {
         // used only with useEffect() 
         // setNumber(n => n + 1)
 
@@ -39,16 +40,42 @@ function MyComponent() {
         //or  
         //ref.current++
 
-        inputRef.current.focus()
-        inputRef.current.style.backgroundColor = "yellow"
+        inputRef1.current.focus()
+        inputRef1.current.style.backgroundColor = "yellow"
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = ""
+    }
+
+    function handleClick2() {
+        inputRef2.current.focus()
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = "yellow"
+        inputRef3.current.style.backgroundColor = ""
+    }
+
+    function handleClick3() {
+        inputRef3.current.focus()
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = "yellow"
     }
 
   return (
     <div>
-        <button onClick={handleClick}>
-            Click me!
+        <button onClick={handleClick1}>
+            Click me 1!
         </button>
-        <input ref={inputRef} />
+        <input ref={inputRef1} />
+
+        <button onClick={handleClick2}>
+            Click me 2!
+        </button>
+        <input ref={inputRef2} />
+
+        <button onClick={handleClick3}>
+            Click me 3!
+        </button>
+        <input ref={inputRef3} />
     </div>
   )
 }
