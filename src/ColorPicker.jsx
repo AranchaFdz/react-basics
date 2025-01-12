9293fe73c5f00f52646bf778f9e6959a3dc9473d
Arancha Fernandez <aranchafernandez.78@gmail.com>
2025-01-12 16:53:10 +0100

    update

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -11,7 +11,8 @@ function ColorPicker() {
   return (
     <div className='color-picker-container'>
         <h1>Color Picker</h1>
-        <div className="color-display" style={{backgroundColor:color}}>
+        {/* // must enclose css properties in an object thus {}  */}
+        <div className="color-display" style={{backgroundColor:color}}> 
             <p>Selected Color: {color}</p>
         </div>
         <label>Select a Color:</label>
